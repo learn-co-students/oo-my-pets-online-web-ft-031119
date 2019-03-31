d530570c15c3f00e0ac4b67693029d658aacff83
ddlou <darren.lou@gmail.com>
2019-03-31 11:50:51 -0700

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -21,7 +21,7 @@ class Owner
   end
 
   def self.reset_all
-    self.all.clear
+    @@all.clear
   end
 
   def say_species
@@ -29,45 +29,43 @@ class Owner
   end
 
   def buy_fish(name)
-    self.pets[:fishes] << Fish.new(name)
+    pets[:fishes] << Fish.new(name)
   end
 
-  def buy_cat(cat)
-    self.pets[:cats] << Cat.new(cat).tap {|c| c.name}
+  def buy_cat(name)
+    pets[:cats] << Cat.new(name)
   end
 
-  def buy_dog(dog)
-    self.pets[:dogs] << Dog.new(dog).tap {|d| d.name}
+  def buy_dog(name)
+    pets[:dogs] << Dog.new(name)
   end
 
   def walk_dogs
-    self.pets[:dogs].each {|dog|
+    pets[:dogs].each {|dog|
       dog.mood = "happy"
     }
   end
 
   def play_with_cats
-    self.pets[:cats].each {|cat|
+    pets[:cats].each {|cat|
       cat.mood = "happy"
     }
   end
 
   def feed_fish
-    self.pets[:fishes].each {|fish|
+    pets[:fishes].each {|fish|
       fish.mood = "happy"
     }
   end
 
   def sell_pets
-    self.pets.each{ |type, pet|
+    pets.each{ |type, pet|
       pet.each{|i| i.mood = "nervous"}
     }
-    self.pets.clear
+    pets.clear
   end
 
   def list_pets
-    
-
-    "I have #{@pets[:fishes].length} fish, #{@pets[:dogs].length} dog(s), and #{@pets[:cats].length} cat(s)."
+    "I have #{pets[:fishes].length} fish, #{pets[:dogs].length} dog(s), and #{pets[:cats].length} cat(s)."
   end
 end
